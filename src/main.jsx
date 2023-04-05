2df0834c6e78e0b9410d45394659388a556ab292
Ziynatilloh <ziynatillomuhammadjonov@gmail.com>
2023-04-05 19:19:43 +0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import * as te from "tw-elements";
-import "./index.css";
+import "./Index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
